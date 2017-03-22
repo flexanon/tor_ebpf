cdaa33c24261e5a226a2377a2de8deeab8098c34
Florentin Rochet <florentin.rochet@uclouvain.be>
2017-03-22 09:51:57 +0100

    diff code

--- src/app/config/config.c
+++ src/app/config/config.c
@@ -662,7 +662,7 @@ static const config_var_t option_vars_[] = {
   V(Schedulers,                  CSV,      "KIST,KISTLite,Vanilla"),
   V(ShutdownWaitLength,          INTERVAL, "30 seconds"),
   OBSOLETE("SocksListenAddress"),
-  V(SignalBlankIntervalMS,       MSEC_INTERVAL, "20 msec"),
+  V(SignalBlankIntervalMS,       MSEC_INTERVAL, "100 msec"),
   V(SignalMethod,                UINT,     "1"),
   V(SocksPolicy,                 LINELIST, NULL),
   VPORT(SocksPort),
