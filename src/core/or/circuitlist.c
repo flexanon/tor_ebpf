efe414e906c19371baf424787225351ba886f8b0
Florentin Rochet <florentin.rochet@uclouvain.be>
2017-04-28 16:06:12 +0200

    debug

--- src/core/or/circuitlist.c
+++ src/core/or/circuitlist.c
@@ -1005,6 +1005,8 @@ init_circuit_base(circuit_t *circ)
 
   smartlist_add(circuit_get_global_list(), circ);
   circ->global_circuitlist_idx = smartlist_len(circuit_get_global_list()) - 1;
+
+  circ->timing_circ_id = 0;
 }
 
 /** If we haven't yet decided on a good timeout value for circuit
