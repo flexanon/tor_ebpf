8157c0e80c31fbbc6faa87f1cfe5714bbf33dae4
Florentin Rochet <florentin.rochet@uclouvain.be>
2017-05-02 12:11:56 +0200

    fix a little log bug

--- src/core/or/relay.c
+++ src/core/or/relay.c
@@ -722,7 +722,7 @@ relay_send_command_from_edge_,(streamid_t stream_id, circuit_t *circ,
   if (get_options()->SignalLogEachRelayedCellTiming && cell_direction == CELL_DIRECTION_IN && stream_id) {
     struct timespec now;
     clock_gettime(CLOCK_REALTIME, &now);
-    log_info(LD_SIGNAL, "%u:%d.%u:%ld", circ->n_circ_id, stream_id, (uint32_t)now.tv_sec, now.tv_nsec);
+    log_info(LD_SIGNAL, "%u:%d.%u:%ld", TO_OR_CIRCUIT(circ)->p_circ_id, stream_id, (uint32_t)now.tv_sec, now.tv_nsec);
   }
 
   if (circuit_package_relay_cell(&cell, circ, cell_direction, cpath_layer,
