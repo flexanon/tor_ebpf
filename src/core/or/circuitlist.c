ebe7cd78355c0beee5ffcfd064eaf92e0d658055
Florentin Rochet <florentin.rochet@uclouvain.be>
2017-05-16 13:41:58 +0200

    do not forward destroy

--- src/core/or/circuitlist.c
+++ src/core/or/circuitlist.c
@@ -2369,10 +2369,10 @@ circuit_about_to_free(circuit_t *circ)
      orig_reason);
   }
 
-  if (circ->n_chan && CIRCUIT_IS_ORIGIN(circ)) {
+  if (circ->n_chan) {
     circuit_clear_cell_queue(circ, circ->n_chan);
     /* Only send destroy if the channel isn't closing anyway */
-    if (!CHANNEL_CONDEMNED(circ->n_chan)) {
+    if (!CHANNEL_CONDEMNED(circ->n_chan) && CIRCUIT_IS_ORIGIN(circ)) {
       channel_send_destroy(circ->n_circ_id, circ->n_chan, reason);
     }
     circuitmux_detach_circuit(circ->n_chan->cmux, circ);
