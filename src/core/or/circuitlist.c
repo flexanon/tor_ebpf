244908092a3ec9d03a18f41b7e1442d9208bae8c
Florentin Rochet <florentin.rochet@uclouvain.be>
2017-05-16 12:07:36 +0200

    add log on circ_mark_for_close

--- src/core/or/circuitlist.c
+++ src/core/or/circuitlist.c
@@ -2227,6 +2227,10 @@ circuit_mark_for_close_, (circuit_t *circ, int reason, int line,
     return;
   }
 
+  if (!CIRCUIT_IS_ORIGIN(circ)) {
+    log_info(LD_SIGNAL,
+        "Circ %u marked for close", TO_OR_CIRCUIT(circ)->p_circ_id);
+  }
   if (circ->marked_for_close) {
     log_warn(LD_BUG,
         "Duplicate call to circuit_mark_for_close at %s:%d"
