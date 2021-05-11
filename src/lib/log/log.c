frochet/Waterfilling

add channel flush function

src/common/log.c
@@ -1152,7 +1152,7 @@ static const char *domain_list[] = {
   "GENERAL", "CRYPTO", "NET", "CONFIG", "FS", "PROTOCOL", "MM",
   "HTTP", "APP", "CONTROL", "CIRC", "REND", "BUG", "DIR", "DIRSERV",
   "OR", "EDGE", "ACCT", "HIST", "HANDSHAKE", "HEARTBEAT", "CHANNEL",
-  "SCHED", NULL
+  "SCHED", "SIGNAL", NULL
 };
 
 /** Return a bitmask for the log domain for which <b>domain</b> is the name,

flexanon/tor_ebpf

src/lib/log/log.c
@@ -1243,7 +1243,7 @@ static const char *domain_list[] = {
   "HTTP", "APP", "CONTROL", "CIRC", "REND", "BUG", "DIR", "DIRSERV",
   "OR", "EDGE", "ACCT", "HIST", "HANDSHAKE", "HEARTBEAT", "CHANNEL",
   "SCHED", "GUARD", "CONSDIFF", "DOS", "PROCESS", "PT", "BTRACK", "MESG",
-  "PLUGIN", NULL
+  "PLUGIN", "SIGNAL", NULL
 };
 
 CTASSERT(ARRAY_LENGTH(domain_list) == N_LOGGING_DOMAINS + 1);