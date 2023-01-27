24c28cfe0aa293691b1cd15913c89aaa65c8e7f5
Florentin Rochet <florentin.rochet@gmail.com>
2023-01-27 11:22:14 +0100

    fixup

--- src/plugins/dropmark_def_conn_based/circuit_unrecognized_data_received.c
+++ src/plugins/dropmark_def_conn_based/circuit_unrecognized_data_received.c
@@ -17,6 +17,9 @@ uint64_t circuit_unrecognized_data_received(relay_process_edge_t *args) {
   cell_t *mycell = my_plugin_malloc(plugin, sizeof(*mycell));
   my_plugin_memcpy(mycell, cell, sizeof(*mycell));
   log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "Plugin: Copied cell Pointer is (%p)", mycell);
+  log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "sizeof(cell_t*): %lu", sizeof(mycell));
+  log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "fifo size: %u", ctx->cell_queue->itemsize);
+  
   queue_ret_t ret = queue_push(ctx->cell_queue, &mycell);
 
   cell_t **pop_cell = NULL;
@@ -24,6 +27,8 @@ uint64_t circuit_unrecognized_data_received(relay_process_edge_t *args) {
   ret  = queue_pop(ctx->cell_queue, data);
   pop_cell = (cell_t **) data;
   log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "Plugin: Pop_cell is is (%p)", *pop_cell);
+  log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "Plugin: data is is (%p)", (cell_t*) *(uint8_t*)data);
+
   ret = queue_push(ctx->cell_queue, &mycell);
 
   if (ret != OK) {
