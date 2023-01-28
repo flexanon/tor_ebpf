6028414757efb033265241db21aaafcc150de4eb
Florentin Rochet <florentin.rochet@gmail.com>
2023-01-28 11:22:55 +0100

    more logging

--- src/plugins/dropmark_def_conn_based/circuit_unrecognized_data_received.c
+++ src/plugins/dropmark_def_conn_based/circuit_unrecognized_data_received.c
@@ -31,6 +31,7 @@ uint64_t circuit_unrecognized_data_received(relay_process_edge_t *args) {
   cell_t *pop_cell = NULL;
   ret  = queue_pop(ctx->cell_queue, &pop_cell);
   log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "Plugin: Pop_cell is  (%p)", pop_cell);
+  log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "Plugin: Pop_cell u64 is  (%lu)", (uint64_t) pop_cell);
   log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "Plugin: data is in fifo (%p)", *(cell_t**) &ctx->cell_queue->queue[0]);
   log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "Plugin: pop_cell command is %u", pop_cell->command);
   log_fn_(LOG_DEBUG, LD_PLUGIN, __FUNCTION__, "Plugin: pop_cell circid is %u", pop_cell->circ_id);
